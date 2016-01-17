11b759a4642f79093e5c440584508a368fb24ae6
James Stevenson <everamzah@gmail.com>
2016-01-17 07:23:02 -0500

    Start furnace conversion

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 --the claycrafter is supposed to be some sort of furnace
 --that uses compressed dirt and glasses of water to make clay, it's not needed to use the mod currently.
- 
+
+--[[ 
 minetest.register_node("claycrafter:claycrafter", {
 		description = "Clay crafter",
 		tiles = {
@@ -13,6 +14,7 @@ minetest.register_node("claycrafter:claycrafter", {
 				},
 		groups = {oddly_breakable_by_hand = 1, choppy = 1}
 })
+--]]
 
 --Compressed dirt
 minetest.register_node("claycrafter:compressed_dirt", {
@@ -30,11 +32,18 @@ minetest.register_craft({
 })
 
 --Glass of water
+-- Water glasses, like full buckets of water, might
+-- should have, stack_max = 1
 minetest.register_craftitem("claycrafter:glass_of_water", {
 		description = "Glass of Water",
-		inventory_image = "claycrafter_glass_of_water.png"
+		inventory_image = "claycrafter_glass_of_water.png",
+		groups = {h2o = 1}, -- How much compressed dirt will turn to clay
+		--stack_max = 1
 })
 
+-- Better may be to override the vessels:drinking_glass item
+-- and allow scooping water just as the bucket does
+-- (Always wanted to do that.)
 minetest.register_craft({
 		output = "claycrafter:glass_of_water 8",
 		recipe = {
@@ -56,3 +65,14 @@ minetest.register_craft({
 				{"claycrafter:glass_of_water", "vessels:drinking_glass"},
 		}
 })
+
+--[[
+minetest.register_craft({
+		type = "h2o", -- normal, cooking,or fuel only
+		output = "default:clay",
+		recipe = {"claycrafter:compressed_dirt"},
+		recplacements = {{"claycrafter:glass_of_water"}}
+})
+--]]
+
+dofile(minetest.get_modpath("claycrafter") .. "/claycrafter.lua")
