2235a0390090f47a9d6b77f5e8dca1358d609662
Dragonop <Dragonop@users.noreply.github.com>
2016-01-19 06:42:41 -0300

    Deleted comments in init.lua
    Deleted comments in init.lua and changed h2o = 4 to h2o = 1

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
--- the claycrafter is supposed to be some sort of furnace
--- that uses compressed dirt and glasses of water to make clay,
--- it's not needed to use the mod currently.
-
-
 --Compressed dirt
 minetest.register_node("claycrafter:compressed_dirt", {
 		description = "Compressed Dirt",
@@ -20,18 +15,14 @@ minetest.register_craft({
 })
 
 --Glass of water
--- Water glasses, like full buckets of water, might
--- should have, stack_max = 1
+-- Water glasses, might should have stack_max = 1, change this if you preffer
 minetest.register_craftitem("claycrafter:glass_of_water", {
 		description = "Glass of Water",
 		inventory_image = "claycrafter_glass_of_water.png",
-		groups = {h2o = 4}, -- How much compressed dirt will turn to clay
+		groups = {h2o = 1}, -- How much compressed dirt will turn to clay
 		--stack_max = 1
 })
 
--- Better may be to override the vessels:drinking_glass item
--- and allow scooping water just as the bucket does
--- (Always wanted to do that.)
 minetest.register_craft({
 		output = "claycrafter:glass_of_water 8",
 		recipe = {
@@ -52,32 +43,5 @@ minetest.register_craft({
 			{"default:stick", "bucket:bucket_water", "default:stick"},
 		}
 })
-
---Craft the clay
---[[
-minetest.register_craft({
-		type = "shapeless",
-		output = "default:clay 4",
-		recipe = {"claycrafter:compressed_dirt", "claycrafter:glass_of_water"},
-		replacements = {
-			{"claycrafter:glass_of_water", "vessels:drinking_glass"},
-		}
-})
---]]
-
--- Keeping this here for reference
--- As there are two ways a regular furnace gets fuel time
--- One, fron a craft recipe like this with "fuel" or "cooking"
--- type, and second from the fuel item group rating.
-
--- Block comment follows
---[[
-minetest.register_craft({
-		type = "h2o", -- normal, cooking,or fuel only
-		output = "default:clay",
-		recipe = {"claycrafter:compressed_dirt"},
-		recplacements = {{"claycrafter:glass_of_water"}}
-})
---]]
 
 dofile(minetest.get_modpath("claycrafter") .. "/claycrafter.lua")
