7a8d7ea8c6b7ebab0a5a03ae918fcf2040ccb966
Dragonop <Dragonop@users.noreply.github.com>
2016-01-19 16:10:59 -0300

    Vessel group
    Added vessel group to glass of water.

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ minetest.register_craft({
 minetest.register_craftitem("claycrafter:glass_of_water", {
 		description = "Glass of Water",
 		inventory_image = "claycrafter_glass_of_water.png",
-		groups = {h2o = 1}, -- How much time to convert 1 compressed dirt to 4 clay
+		groups = {h2o = 1, vessel = 1}, -- How much time to convert 1 compressed dirt to 4 clay
 })
 
 minetest.register_craft({
