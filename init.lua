d241c8d8692c0b01493b97e59932b8e31d187f83
Dragonop <Dragonop@users.noreply.github.com>
2016-01-24 07:08:39 -0300

    More groups for compressed dirt

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 minetest.register_node("claycrafter:compressed_dirt", {
 		description = "Compressed Dirt",
 		tiles = {"claycrafter_compressed_dirt.png"},
-		groups = {crumbly = 1, oddly_breakable_by_hand = 1}
+		groups = {crumbly = 1, oddly_breakable_by_hand = 1, soil = 1, cracky =1}
 })
 -- Crafts
 minetest.register_craft({
