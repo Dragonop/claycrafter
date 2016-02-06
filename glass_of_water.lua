c8c4e41cae63b6e0837ccfe246eea1deaabe4ec5
James Stevenson <james@ton.din>
2016-02-06 17:51:51 -0500

    Move glass of water def to items.lua

--- glass_of_water.lua
+++
@@ -1,5 +0,0 @@
-minetest.register_craftitem("claycrafter:glass_of_water", {
-		description = "Glass of Water",
-		inventory_image = "claycrafter_glass_of_water.png",
-		groups = {h2o = 1, vessel = 1}, -- How much time to convert 1 compressed dirt to 4 clay
-})
--- items.lua
+++ items.lua
@@ -3,3 +3,9 @@ minetest.register_node("claycrafter:compressed_dirt", {
 		tiles = {"claycrafter_compressed_dirt.png"},
 		groups = {crumbly = 1, oddly_breakable_by_hand = 1, soil = 1, cracky =1}
 })
+
+minetest.register_craftitem("claycrafter:glass_of_water", {
+		description = "Glass of Water",
+		inventory_image = "claycrafter_glass_of_water.png",
+		groups = {h2o = 1, vessel = 1}, -- How much time to convert 1 compressed dirt to 4 clay
+})
