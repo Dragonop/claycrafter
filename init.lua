21e22e4451df2cc60df2b075de435695834e5d03
James Stevenson <james@ton.din>
2016-02-06 17:43:54 -0500

    Empty init.lua of node and craft defs.

--- init.lua
+++ init.lua
@@ -1,40 +1,8 @@
--- Compressed dirt
-minetest.register_node("claycrafter:compressed_dirt", {
-		description = "Compressed Dirt",
-		tiles = {"claycrafter_compressed_dirt.png"},
-		groups = {crumbly = 1, oddly_breakable_by_hand = 1, soil = 1, cracky =1}
-})
--- Crafts
-minetest.register_craft({
-		output = "claycrafter:compressed_dirt",
-		recipe = {
-			{"default:dirt", "default:dirt", "default:dirt"},
-			{"default:dirt", "default:dirt", "default:dirt"},
-			{"default:dirt", "default:dirt", "default:dirt"}
-		}
-})
+local modpath = minetest.get_modpath("claycrafter")
 
-minetest.register_craft({
-		output = "claycrafter:glass_of_water 8",
-		recipe = {
-			{"vessels:drinking_glass", "vessels:drinking_glass", "vessels:drinking_glass"},
-			{"vessels:drinking_glass", "bucket:bucket_water", "vessels:drinking_glass"}, 
-			{"vessels:drinking_glass", "vessels:drinking_glass", "vessels:drinking_glass"}
-		},
-		replacements = {
-			{"bucket:bucket_water", "bucket:bucket_empty"},
-		}
-})
+-- TODO: Investigate function to automatically get mod name.
 
-minetest.register_craft({
-		output = "claycrafter:claycrafter",
-		recipe = {
-			{"group:wood", "default:steel_ingot", "group:wood"},
-			{"group:wood", "default:glass", "group:wood"},
-			{"default:stick", "bucket:bucket_water", "default:stick"}
-		}
-})
-
-
-dofile(minetest.get_modpath("claycrafter") .. "/claycrafter.lua")
-dofile(minetest.get_modpath("claycrafter") .. "/glass_of_water.lua")
+dofile(modpath .. "/items.lua")
+dofile(modpath .. "/recipes.lua")
+dofile(modpath .. "/claycrafter.lua")
+dofile(modpath .. "/glass_of_water.lua")
