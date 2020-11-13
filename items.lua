64aae49bbd71df3bfcf75261608e751346316dba
Joaquin Villalba <Dragonop@users.noreply.github.com>
2020-11-13 00:53:54 -0300

    Glass of water now placeable - nerfed convert time
    Glass of Water is now a placeable node. Can now be used to empty it out, returning an Empty Drinking Glass.
    Don't recomend changing the item_eat value to any other than 0, if you don't want free food.

--- items.lua
+++ items.lua
@@ -4,8 +4,18 @@ minetest.register_node("claycrafter:compressed_dirt", {
 		groups = {crumbly = 1, oddly_breakable_by_hand = 1, soil = 1, cracky =1}
 })
 
-minetest.register_craftitem("claycrafter:glass_of_water", {
-		description = "Glass of Water",
-		inventory_image = "claycrafter_glass_of_water.png",
-		groups = {h2o = 1, vessel = 1}, -- How much time to convert 1 compressed dirt to 4 clay
+minetest.register_node("claycrafter:glass_of_water", {
+	description = ("Glass of Water"),
+	drawtype = "plantlike",
+	tiles = {"claycrafter_glass_of_water.png"},
+	inventory_image = "claycrafter_glass_of_water_inv.png",
+	wield_image = "claycrafter_glass_of_water.png",
+	paramtype = "light",
+	is_ground_content = false,
+	walkable = false,
+	sunlight_propagates = true,
+	selection_box = {type = "fixed",fixed = {-0.25, -0.5, -0.25, 0.25, 0, 0.25}	},
+	groups = {vessel = 1, dig_immediate = 3, attached_node = 1, h2o = 3}, -- How much time to convert 1 compressed dirt to 4 clay
+	on_use = minetest.item_eat(0,"vessels:drinking_glass"),	
+	sounds = default.node_sound_glass_defaults(),
 })
